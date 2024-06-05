e009929c2523444997265cf607482fc87fe1cb7b
sumanth8686 <58932303+sumanth8686@users.noreply.github.com>
2024-06-05 14:30:49 +0530

    u

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -4,6 +4,10 @@ package com.lerner.eureka;
 
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class LernerEurekaApplication {

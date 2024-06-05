36faed891272e89da85b02eca7bb9c02c726d757
sumanth8686 <58932303+sumanth8686@users.noreply.github.com>
2024-06-05 14:25:55 +0530

    removed unsued source code

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -2,14 +2,14 @@ package com.lerner.eureka;
 
 
 
-import org.slf4j.Logger; // for code smells
+/* import org.slf4j.Logger; // for code smells
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; */
 
 @SpringBootApplication
 @EnableEurekaServer
